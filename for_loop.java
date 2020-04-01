028ec3df5261c4b340e04f957c2e03fec8908520
UdaiChauhan284 <udaichauhan284@gmail.com>
2020-04-01 16:47:56 +0530

    initial commit helloWorld and for_loop

---
+++ for_loop.java
@@ -0,0 +1,9 @@
+class for_loop{
+	public static void main(String ar[])
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Hello World");
+		}
+	}
+}
---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+class helloWorld{
+	public static void main(String ar[])
+	{
+		System.out.println("Hello World, I am learning Git");
+	}
+}
